ae44de44c95d6f436a27874b1a50a3377c2c84a0
sanchel0 <santiagodanielbravo1820@gmail.com>
2025-05-20 23:07:00 -0300

    Add API constants and .env.development configuration for API base URL

---
+++ Frontend/src/constants/api.js
@@ -0,0 +1,4 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const API_URL_ACTORS = `${API_BASE_URL}/actors`;
+export const API_URL_DIRECTORS = `${API_BASE_URL}/directors`;
+export const API_URL_GENRES = `${API_BASE_URL}/genres`;
